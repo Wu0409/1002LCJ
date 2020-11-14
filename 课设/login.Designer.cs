a9d0ad7e82d26edc531666269c2b180f69d7c26e
吴远尘 <wyc872083177@gmail.com>
2020-11-14 18:03:05 +0800

    测试

---
+++ 课设/login.Designer.cs
@@ -0,0 +1,157 @@
+﻿
+namespace 课设
+{
+    partial class login
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.title = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.ID = new System.Windows.Forms.Label();
+            this.id_shuru = new System.Windows.Forms.TextBox();
+            this.password = new System.Windows.Forms.Label();
+            this.password_shuru = new System.Windows.Forms.TextBox();
+            this.Denglu = new System.Windows.Forms.Button();
+            this.Zhuce = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // title
+            // 
+            this.title.AutoSize = true;
+            this.title.Font = new System.Drawing.Font("微软雅黑", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.title.Location = new System.Drawing.Point(59, 29);
+            this.title.Name = "title";
+            this.title.Size = new System.Drawing.Size(219, 31);
+            this.title.TabIndex = 0;
+            this.title.Text = "订单管理系统 1.0.0";
+            this.title.TextAlign = System.Drawing.ContentAlignment.BottomRight;
+            // 
+            // listBox1
+            // 
+            this.listBox1.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 27;
+            this.listBox1.Items.AddRange(new object[] {
+            "用户端",
+            "商家端",
+            "管理端"});
+            this.listBox1.Location = new System.Drawing.Point(63, 80);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(213, 31);
+            this.listBox1.TabIndex = 1;
+            // 
+            // ID
+            // 
+            this.ID.AutoSize = true;
+            this.ID.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.ID.Location = new System.Drawing.Point(60, 134);
+            this.ID.Name = "ID";
+            this.ID.Size = new System.Drawing.Size(72, 27);
+            this.ID.TabIndex = 2;
+            this.ID.Text = "账号：";
+            // 
+            // id_shuru
+            // 
+            this.id_shuru.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.id_shuru.Location = new System.Drawing.Point(63, 164);
+            this.id_shuru.Name = "id_shuru";
+            this.id_shuru.Size = new System.Drawing.Size(213, 34);
+            this.id_shuru.TabIndex = 3;
+            // 
+            // password
+            // 
+            this.password.AutoSize = true;
+            this.password.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.password.Location = new System.Drawing.Point(60, 211);
+            this.password.Name = "password";
+            this.password.Size = new System.Drawing.Size(72, 27);
+            this.password.TabIndex = 4;
+            this.password.Text = "密码：";
+            // 
+            // password_shuru
+            // 
+            this.password_shuru.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.password_shuru.Location = new System.Drawing.Point(63, 241);
+            this.password_shuru.Name = "password_shuru";
+            this.password_shuru.Size = new System.Drawing.Size(213, 34);
+            this.password_shuru.TabIndex = 5;
+            this.password_shuru.UseSystemPasswordChar = true;
+            // 
+            // Denglu
+            // 
+            this.Denglu.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.Denglu.Location = new System.Drawing.Point(63, 301);
+            this.Denglu.Name = "Denglu";
+            this.Denglu.Size = new System.Drawing.Size(85, 35);
+            this.Denglu.TabIndex = 6;
+            this.Denglu.Text = "登录";
+            this.Denglu.UseVisualStyleBackColor = true;
+            this.Denglu.Click += new System.EventHandler(this.Denglu_Click);
+            // 
+            // Zhuce
+            // 
+            this.Zhuce.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.Zhuce.Location = new System.Drawing.Point(191, 301);
+            this.Zhuce.Name = "Zhuce";
+            this.Zhuce.Size = new System.Drawing.Size(85, 35);
+            this.Zhuce.TabIndex = 7;
+            this.Zhuce.Text = "注册";
+            this.Zhuce.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(335, 399);
+            this.Controls.Add(this.Zhuce);
+            this.Controls.Add(this.Denglu);
+            this.Controls.Add(this.password_shuru);
+            this.Controls.Add(this.password);
+            this.Controls.Add(this.id_shuru);
+            this.Controls.Add(this.ID);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.title);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label title;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label ID;
+        private System.Windows.Forms.TextBox id_shuru;
+        private System.Windows.Forms.Label password;
+        private System.Windows.Forms.TextBox password_shuru;
+        private System.Windows.Forms.Button Denglu;
+        private System.Windows.Forms.Button Zhuce;
+    }
+}
+
---
+++ 课设/login.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data;
+using SqlSugar;
+
+
+namespace 课设
+{
+    public partial class login : Form
+    {
+        public login()
+        {
+            InitializeComponent();
+        }
+
+        // _______________定义user模型___________________  2020.11.14 吴远尘
+        [SugarTable("user")]
+        public class Usermodel
+        {
+            //指定主键和自增列，当然数据库中也要设置主键和自增列才会有效
+            [SugarColumn(IsPrimaryKey = true, IsIdentity = true)]
+            public string id { get; set; }
+            public string password { get; set; }
+        }
+
+        private void Denglu_Click(object sender, EventArgs e)
+        {
+            string id = id_shuru.Text;  // 获取用户输入的账号
+
+            SqlSugarClient db = new SqlSugarClient(new ConnectionConfig()
+            {
+                ConnectionString = "server=localhost;uid=root;pwd=1234567890;database=db",
+                DbType = SqlSugar.DbType.MySql,//设置数据库类型
+                IsAutoCloseConnection = true,//自动释放数据务，如果存在事务，在事务结束后释放
+                InitKeyType = InitKeyType.Attribute //从实体特性中读取主键自增列信息
+            });
+
+            var getByWhere = db.Queryable<Usermodel>().Where(it => it.id == id).ToList();//根据条件查询
+            
+            MessageBox.Show(getByWhere[0].password);
+            
+
+        }
+
+
+    }
+}
